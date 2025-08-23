a8666510a0592bd78bd8ea59beaed15924aca963
Javier Alexander Plata Rueda <jatapsilver92@hotmail.com>
2025-08-23 15:17:06 -0600

    createSmartContract

---
+++ smartContract/hardhat.config.ts
@@ -0,0 +1,34 @@
+import type { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import "dotenv/config";
+
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
+
+const config: HardhatUserConfig = {
+  solidity: {
+    version: "0.8.28",
+    settings: {
+      metadata: {
+        bytecodeHash: "none", // disable ipfs
+        useLiteralContent: true, // use source code
+      },
+    },
+  },
+  networks: {
+    monadTestnet: {
+      url: "https://testnet-rpc.monad.xyz",
+      accounts: PRIVATE_KEY ? [PRIVATE_KEY] : [],
+      chainId: 10143,
+    },
+  },
+  etherscan: {
+    enabled: false,
+  },
+  sourcify: {
+    enabled: true,
+    apiUrl: "https://sourcify-api-monad.blockvision.org",
+    browserUrl: "https://testnet.monadexplorer.com",
+  },
+};
+
+export default config;
